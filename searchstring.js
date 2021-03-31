54c1a8485a02c95028d162df585a438c567b1600
Vaughn Moriniere <vaughnmoriniere@yahoo.com>
2021-03-31 16:06:40 -0500

    Least and Most used words added and successfully loads the book

--- searchstring.js
+++ searchstring.js
@@ -10,12 +10,14 @@ function loadBook(filename, displayName) {
 
     //create a server request
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true );
+    xhr.open("GET", url, true);
     xhr.send();
 
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
             currentBook = xhr.responseText;
+
+            getDocStats(currentBook);
 
             //remove line breaks and stuff
             currentBook = currentBook.replace(/(?:\r\n|\r|\n)/g, '<br>');
@@ -31,7 +33,7 @@ function loadBook(filename, displayName) {
 function getDocStats(fileContent) {
     var docLength = document.getElementById("docLength");
     var wordCount = document.getElementById("wordCount");
-    var charCount = document.getElementById('charCount');
+    var charCount = document.getElementById("charCount");
 
     let text = fileContent.toLowerCase();
     let wordArray = text.match(/\b\S+\b/g); //returns an array of words if there is a space on both sides.
@@ -42,19 +44,50 @@ function getDocStats(fileContent) {
         let wordValue = wordArray[word];
         if (wordDictionary[wordValue] > 0) {
             wordDictionary[wordValue] += 1;
-        }
-        else {
+        } else {
             wordDictionary[wordValue] = 1; // this part is mainly for the first time it find the word.
         }
     }
+    //sort array
+    let wordList = sortProperties(wordDictionary);
+
+    //return the top 5 words
+    var top5Words = wordList.slice(0, 6);
+
+    //return the least 5 words
+    var least5Words = wordList.slice(-6, wordList.length);
+
+    //write these to the page
+    ULTemplate(top5Words, document.getElementById('mostUsed'));
+    ULTemplate(least5Words, document.getElementById('leastUsed'));
+
 }
+
+function ULTemplate(items, element) {
+    let rowTemplate = document.getElementById('template-ul-items');
+    let templateHTML = rowTemplate.innerHTML;
+    let resultsHTML = "";
+
+    for (i = 0; i < items.length - 1; i++) {
+        resultsHTML += templateHTML.replace("{{val}}", items[i][0] + ":" + items[i][1] + 'time(s)');
+    }
+
+    element.innerHTML = resultsHTML;
+}
+
+
+
 
 //function that sorts the most and least used words
 
 function sortProperties(obj) {
     //first convert the object to an array
-    let rtnArray = Object.defineProperties(obj);
+    let rtnArray = Object.entries(obj);
 
     //sort the array
-    
+    rtnArray.sort(function (first, second) {
+        return second[1] - first[1];
+    });
+
+    return rtnArray;
 }
